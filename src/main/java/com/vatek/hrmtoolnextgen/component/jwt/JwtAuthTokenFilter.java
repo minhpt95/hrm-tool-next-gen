e34f8d293f3923e908ee697a7baf5531b7f83830
Mark Pham <mark.pham@vatek.asia>
2025-11-14 09:33:59 +0700

    Refactor

--- src/main/java/com/vatek/hrmtoolnextgen/component/jwt/JwtAuthTokenFilter.java
+++ src/main/java/com/vatek/hrmtoolnextgen/component/jwt/JwtAuthTokenFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.apache.commons.lang3.StringUtils;

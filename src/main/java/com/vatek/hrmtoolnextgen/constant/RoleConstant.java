f106ec815e3c2a7fa4e0e91388751b291ac34244
Mark Pham <mark.pham@vatek.asia>
2025-12-11 14:40:14 +0700

    Refactor Role Constants and Remove InitDataService
    - Updated role constants in RoleConstant.java to use simplified naming without the "ROLE_" prefix.
    - Removed InitDataService.java as it was no longer needed for role initialization.

--- src/main/java/com/vatek/hrmtoolnextgen/constant/RoleConstant.java
+++ src/main/java/com/vatek/hrmtoolnextgen/constant/RoleConstant.java
@@ -1,9 +1,9 @@
 package com.vatek.hrmtoolnextgen.constant;
 
 public class RoleConstant {
-    public static final String ADMIN = "ROLE_ADMIN";
-    public static final String IT_ADMIN = "ROLE_IT_ADMIN";
-    public static final String PROJECT_MANAGER = "ROLE_PROJECT_MANAGER";
-    public static final String USER = "ROLE_USER";
-    public static final String HR = "ROLE_HR";
+    public static final String ADMIN = "ADMIN";
+    public static final String IT_ADMIN = "IT_ADMIN";
+    public static final String PROJECT_MANAGER = "PROJECT_MANAGER";
+    public static final String USER = "USER";
+    public static final String HR = "HR";
 }

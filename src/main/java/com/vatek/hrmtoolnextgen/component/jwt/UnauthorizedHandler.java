bf679fe46518ff4882f606335fc246f41d36afa0
Mark Pham <mark.pham@vatek.asia>
2025-12-11 18:53:15 +0700

    Enhance UnauthorizedHandler for Improved Error Responses
    - Added ObjectMapper and CommonErrorResponse to the UnauthorizedHandler class.
    - Updated the commence method to return a standardized JSON error response with HTTP status 401 (Unauthorized) and relevant error details, improving error handling for unauthorized access.

--- src/main/java/com/vatek/hrmtoolnextgen/component/jwt/UnauthorizedHandler.java
+++ src/main/java/com/vatek/hrmtoolnextgen/component/jwt/UnauthorizedHandler.java
@@ -1,9 +1,12 @@
 package com.vatek.hrmtoolnextgen.component.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vatek.hrmtoolnextgen.dto.response.CommonErrorResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -15,6 +18,8 @@ import java.io.IOException;
 @AllArgsConstructor
 public class UnauthorizedHandler implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
@@ -22,5 +27,17 @@ public class UnauthorizedHandler implements AuthenticationEntryPoint {
             throws IOException {
 
         log.error("Unauthorized error. Message : {} , {}", request.getRequestURL(), e);
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+
+        CommonErrorResponse body = CommonErrorResponse
+                .commonErrorResponseBuilder()
+                .message(e.getMessage())
+                .httpStatusCode(HttpStatus.UNAUTHORIZED)
+                .path(request.getRequestURI())
+                .build();
+
+        response.getWriter().write(objectMapper.writeValueAsString(body));
     }
 }
